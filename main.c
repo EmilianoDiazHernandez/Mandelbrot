2c41d2013badeb33068c10f120fd0aa4a1cfc70e
Francisco Isaac Ordoñez Pedrero <115902650+PaikLC@users.noreply.github.com>
2025-06-25 19:38:37 -0600

    Posible mejora en el codigo para esto y eso... (#2)
    Co-authored-by: Francisco Isaac Ordoñez Pedrero <mri@mri-server.com>

--- main.c
+++ main.c
@@ -1,46 +1,94 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "render.h"
 
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS);
 
     const int width = 800, height = 600;
+
     SDL_Window *window = SDL_CreateWindow("Mandelbrot", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (window == NULL)
+    {
+        printf("\nNo se pudo crear la ventana\n%s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == NULL)
+    {
+        printf("\nNo se pudo crear el render\n%s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        return EXIT_FAILURE;
+    }
 
     Viewport vp = { .min_real = -2.0, .max_real = 1.0, .min_imag = -1.5, .max_imag = 1.5, .max_iter = 500 };
 
     bool needs_redraw = true;
     bool running = true;
 
+    int mouse_x = 0, mouse_y = 0;
+
     SDL_Event event;
     while (running) {
-        SDL_PollEvent(&event);
-        if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
-            running = false;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    running = false;
+                    break;
 
-        if (event.type == SDL_KEYDOWN) {
-            int mouse_x, mouse_y;
-            SDL_GetMouseState(&mouse_x, &mouse_y);
-            
-            if (event.key.keysym.sym == SDLK_PLUS) {
-                zoom_viewport(&vp, mouse_x, mouse_y, width, height, 1.2);
-                needs_redraw = true;
+                case SDL_MOUSEMOTION:
+                    SDL_GetMouseState(&mouse_x, &mouse_y);
+                    break;
+
+                case SDL_KEYDOWN:
+                    {
+                        switch (event.key.keysym.sym)
+                        {
+                            case SDLK_ESCAPE:
+                                running = false;
+                                break;
+
+                            case SDLK_PLUS:
+                                zoom_viewport(&vp, mouse_x, mouse_y, width, height, 1.2);
+                                needs_redraw = true;
+                                break;
+
+                            case SDLK_MINUS:
+                                zoom_viewport(&vp, mouse_x, mouse_y, width, height, 0.8);
+                                needs_redraw = true;
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
             }
-            if (event.key.keysym.sym == SDLK_MINUS) {
-                zoom_viewport(&vp, mouse_x, mouse_y, width, height, 0.8);
-                needs_redraw = true;
+
+            if (needs_redraw && running) {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+                draw_mandelbrot(renderer, width, height, &vp);
+                SDL_RenderPresent(renderer);
+                needs_redraw = false;
             }
         }
-
         if (needs_redraw && running) {
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-            SDL_RenderClear(renderer);
-            draw_mandelbrot(renderer, width, height, &vp);
-            SDL_RenderPresent(renderer);
-            needs_redraw = false;
-        }
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+                draw_mandelbrot(renderer, width, height, &vp);
+                SDL_RenderPresent(renderer);
+                needs_redraw = false;
+            }
     }
 
     SDL_DestroyRenderer(renderer);
